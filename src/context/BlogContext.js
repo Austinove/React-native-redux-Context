88ff209772ddd14d90c1d432d1cfd6fef61c75ab
Bryan Austin <bryanovicaustenove@gmail.com>
2022-09-13 02:14:52 +0300

    failled in actions

---
+++ src/context/BlogContext.js
@@ -0,0 +1,21 @@
+import createDataContex from './createDataContex';
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, {title: `Blog Post #${state.length + 1}`}];
+
+    default:
+      return state;
+  }
+};
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({type: 'add_blogpost'});
+  };
+};
+export const {Context, Provider} = createDataContex(
+  blogReducer,
+  addBlogPost,
+  [],
+);
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useCallback, useContext} from 'react';
+import {View, Text, Button, FlatList} from 'react-native';
+import {Context} from '../context/BlogContext';
+//for many context files imports you use a reference e.g
+//import { Context as BlogContext } from '../context/BlogContext';
 
 const Home = ({navigation}) => {
+  const {state, addBlogPost} = useContext(Context);
   return (
     <View>
       <Text style={{fontSize: 25}}>Testing App</Text>
+      <Button onPress={addBlogPost()} title="Add Blog" />
+      <FlatList
+        data={state}
+        keyExtractor={state => state.title}
+        renderItem={({item}) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
       <Button
         onPress={() => navigation.navigate('Profile', {id: 'none'})}
         title="Route"
